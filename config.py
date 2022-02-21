2a529a26586644e61f7d011ee103ede6f3d47697
amfchef <jakobtj@hotmail.com>
2022-02-21 20:50:57 +0100

    testar igen

---
+++ config.py
@@ -0,0 +1,16 @@
+import boto3
+import json
+
+#boto_client = boto3.client('secretsmanager')
+#response = boto_client.get_secret_value(SecretId='API')
+#database_secrets = json.loads(response['SecretString'])
+
+# Store secrets in AWS
+#API_KEY = database_secrets['API_KEY']
+#API_SECRET = database_secrets['API_SECRET']
+#WEBHOOK_PASSWORD = database_secrets['WEBHOOK_PASSWORD']
+
+
+API_KEY = 'XyC4z59YK8gk9RqJVSgSHouFnXnZIdsPxgOwisWk2p2A1OzGkFEnQxqnPRTSmzm9'
+API_SECRET = '4kpALMPoITLtRJymSwvpDtaPdXJAPPe8xWUsWPZB3VC2BK5sQIDdG4Y5DtCRu4kf'
+WEBHOOK_PASSWORD = 'abc123'
--- server.py
+++ server.py
@@ -1,28 +1,128 @@
-from flask import Flask, jsonify
+from calculate import Calculate
+import json, config
+from werkzeug.utils import redirect
+from flask import Flask, request, render_template
+import datetime
+
+from binance.client import Client
+client = Client(config.API_KEY, config.API_SECRET)
 
 app = Flask(__name__)
+calculate = Calculate()
 
-items = [
-    {
-        'name': 'Table',
-        'price': 5000
-    },
-    {
-        'name': 'Chair',
-        'price': 3000
-    }
-]
+@app.route('/', methods=['GET', 'POST'])
+def welcome():
 
-@app.route('/')
-def index():
-    # return {'message': 'Working fine..'}
-    return "Hellor world"
+    # Calculate all dashboard values
+    calculate.update_current_profit()
+    current_profit = calculate.get_total_current_profit()
+    current_year = datetime.datetime.now().year
+    running_trades = calculate.get_running_trades()
+    all_trades = calculate.get_all_trades()
+    total_profit = int(calculate.get_total_profit())
+    usdt_balance = calculate.get_usdt_balance()
+
+    if request.method == "POST":
+        data = request.form
+        password = data["password"]
+
+        # Check if password is correct with config.py
+        if password == config.WEBHOOK_PASSWORD:
+            # print(data)
+            calculate.append_running_trades(data["coinpair"], int(data["interval"]), float(data["quantity"]),
+                                            data["portion_size"], data["side"],
+                                            sl_id="manual trades, No SL could be set")
+            calculate.get_running_trades()
+
+            return redirect('/')
+        else:
+            return redirect('/')
+    return render_template('index.html', current_profit=current_profit, usdt_balance=usdt_balance,
+                           all_trades=all_trades, total_profit=total_profit, current_year=current_year,
+                           running_trades=running_trades)
 
 
-@app.route('/items')
-def fetch_items():
-    return jsonify(items=items)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = {}
+    try:
+        data = json.loads(request.data)
+    except Exception as e:
+        print("an exception occured - {}".format(e))
+        return {"code": "error",
+                "message": "Unable to read webhook"}
+    # print(request.data)
+
+    # Check if password is correct with config.py
+    if data['password'] != config.WEBHOOK_PASSWORD:
+        return {
+            "code": "error",
+            "message": "Nice try, invalid password"
+        }
+    # Saving webhook values to variables
+    interval = data['interval']
+    coin_pair = data['ticker']
+    signal = data['signal']
+    stop_price = 0
+    if signal == "ENTRY LONG" or signal == "ENTRY SHORT":
+        stop_price = data['stopprice']
+        entry_price = data['entryprice']
+        usdt_balance = calculate.get_usdt_balance()
+
+        if signal == "ENTRY LONG":
+            sl_percent = round((1 - (stop_price / entry_price)), 4)
+            portion_size = calculate.portion_size(
+                usdt_balance, sl_percent)
+
+            quantity = calculate.convert_portion_size_to_quantity(
+                coin_pair, portion_size)
+
+            side = "BUY"
+            order_response = calculate.long_order(side, quantity,
+                                                  coin_pair, interval, portion_size, stop_price, sl_percent)
+
+        elif signal == "ENTRY SHORT":
+            sl_percent = round(((stop_price / entry_price) - 1), 4)
+            portion_size = calculate.portion_size(
+                usdt_balance, sl_percent)
+            quantity = calculate.convert_portion_size_to_quantity(
+                coin_pair, portion_size)
+            side = "SELL"
+            order_response = calculate.short_order(side, quantity,
+                                                   coin_pair, interval, portion_size, stop_price, sl_percent)
+
+    elif signal == "EXIT LONG":
+        side = "SELL"
+        quantity = 0
+        portion_size = 0
+        sl_percentage = 0
+        order_response = calculate.long_order(side, quantity,
+                                              coin_pair, interval, portion_size, stop_price, sl_percentage)
+
+    elif signal == "EXIT SHORT":
+        side = "BUY"
+        quantity = 0
+        portion_size = 0
+        sl_percentage = 0
+        order_response = calculate.short_order(side, quantity,
+                                               coin_pair, interval, portion_size, stop_price, sl_percentage)
+    else:
+        return "An error occured, can read the signal"
+
+    if order_response:
+        return {
+            "code": "success",
+            "message": "order executed"
+        }
+    else:
+        print("order failed")
+
+        return {
+            "code": "error",
+            "message": "order failed"
+
+        }
